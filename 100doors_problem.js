84f7de71ac082901fdad537e7f2c9b98c67eef4b
camimihalescu <33200596+camimihalescu@users.noreply.github.com>
2019-03-14 15:34:16 +0200

    Create 100doors_problem.js

---
+++ 100doors_problem.js
@@ -0,0 +1,6 @@
+echo "# rosseta-problems" >> 100doors_problem.js
+git init
+git add 100doors_problem.js
+git commit -m "first commit"
+git remote add origin git@github.com:camimihalescu/rosseta-problems.git
+git push -u origin master
